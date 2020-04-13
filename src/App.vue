d3f2b3bf91bd185add701ff6e4145a168b4883bb
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-04-13 16:44:09 +0800

    12 divs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
-  <div id="app">
-   
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
+	export default {
 
-export default {
- 
-}
+	}
 </script>
 
 <style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
 
+	a {
+		text-decoration: inherit;
+	}
+
+	em {
+		font-style: normal;
+	}
+
+	ul,
+	ol {
+		list-style: none;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4 {
+		font: inherit;
+	}
+
+	input,
+	select,
+	button {
+		font: inherit;
+	}
+
+	html,
+	body {
+		height: 100%;
+		-webkit-user-select: none;
+		-webkit-touch-callout: none;
+		-webkit-tap-highlight-color: transparent;
+	}
+
+	body {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		background-image: linear-gradient(45deg, #f4f1ff, #fff5f5);
+	}
+
+	#app {
+		height: 100%;
+		overflow: hidden;
+	}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="home">
+		<div><circle-loading></circle-loading></div>
+		<div><badge></badge></div>
+		<div><bubble-loading></bubble-loading></div>
+		<div><calendar></calendar></div>
+		<div><floating-button></floating-button></div>
+		<div><heart></heart></div>
+		<div><land-mark></land-mark></div>
+		<div><mail></mail></div>
+		<div><play-button></play-button></div>
+		<div><progress-bar></progress-bar></div>
+		<div><teacup-loading></teacup-loading></div>
+		<div><vue></vue></div>
+	</div>
+</template>
+
+<script>
+	import CircleLoading from '../components/CircleLoading.vue'
+	import Badge from '../components/Badge.vue'
+	import BubbleLoading from '../components/BubbleLoading.vue'
+	import Calendar from '../components/Calendar.vue'
+	import FloatingButton from '../components/FloatingButton.vue'
+	import Heart from '../components/Heart.vue'
+	import LandMark from '../components/LandMark.vue'
+	import Mail from '../components/Mail.vue'
+	import PlayButton from '../components/PlayButton.vue'
+	import ProgressBar from '../components/ProgressBar.vue'
+	import TeacupLoading from '../components/TeacupLoading.vue'
+	import Vue from '../components/Vue.vue'
+	
+	export default {
+		components: {
+			CircleLoading,
+			Badge,
+			BubbleLoading,
+			Calendar,
+			FloatingButton,
+			Heart,
+			LandMark,
+			Mail,
+			PlayButton,
+			ProgressBar,
+			TeacupLoading,
+			Vue
+		}
+	}
+</script>
+
+<style>
+	.home {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		margin: 0 auto;
+	}
+	.home > div {
+	  transform: scale(1.5);
+		margin: 20px 50px
+	}
+</style>
